WebKit/WebKit

[GStreamer][MSE] Avoid spurious seek to 0 when seeking to end https://bugs.webkit.org/show_bug.cgi?id=320981
Reviewed by Xabier Rodriguez-Calvar.

On a device where a GStreamer seek operation leads to a pipeline async
state change, we may enter a flow of internal calls that cause a second
seek to zero to be executed before the first call has returned. This was
detected with Spotify when performing a seek to the asset end (duration)
right after playback has started from the beginning (position zero).
This issue does not happen if the GStreamer seek operation does not
cause a async pipeline state change.

See: https://github.com/WebPlatformForEmbedded/WPEWebKit/issues/1645

This patch avoids automatic playback when the video has ended
(effectively helping to avoid currentTime accidentally going beyond the
duration) or when a seek is still happening (avoiding currentTime to
progress and change the conditions of the video). ReadyState is
propagated to the player when seek has completed, helping the
HTMLMediaElement algorithms to take the right decisions when seek
completes. Finally, code has been added to avoid the position to ever go
beyond the duration in order to avoid a degradation of the ReadyState to
HaveMetadata, except when the duration is zero. This is a special case
used by MediaPlayerPrivateGStreamer::duration() and
MediaPlayerPrivateGStreamerMSE::duration() when the duration is
unknown/unavailable, and we don't want to mess up with that and cause a
stream that is just being loaded to trigger unexpected effects.

Co-authored-by: Enrique Ocaña González <eocanha@igalia.com>

Test: media/media-source/media-source-seek-to-end-doesnt-loop.html

* LayoutTests/media/media-source/media-source-seek-to-end-doesnt-loop-expected.txt: Added.
* LayoutTests/media/media-source/media-source-seek-to-end-doesnt-loop.html: Added.
* Source/WebCore/html/HTMLMediaElement.cpp:
(WebCore::HTMLMediaElement::mediaPlayerPlaybackStateChanged): Avoid automatic playback on seek or video end

Source/WebCore/html/HTMLMediaElement.cpp
@@ -6510,8 +6510,11 @@ void HTMLMediaElement::mediaPlayerPlaybackStateChanged()
     beginProcessingMediaPlayerCallback();
     if (playerPaused)
         pauseInternal();
-    else
+    else if (!seeking() || !isEnded()) {
+        // The extra conditions above prevent autoplay when seeking, which would start playback from 0
+        // and cause a spurious seek to 0 when seeking to the end of the media.
         playInternal();
+    }
     endProcessingMediaPlayerCallback();
 }
 

Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
@@ -1748,6 +1748,14 @@ MediaTime MediaPlayerPrivateGStreamer::playbackPosition() const
     else if (m_canFallBackToLastFinishedSeekPosition)
         playbackPosition = m_seekTarget.time;
 
+    // Avoid exceeding the reported duration even by minuscule amounts.
+    // The multiplatform layer could degrade us to HaveMetadata if we let that happen (e.g. in MSE monitorSourceBuffers()).
+    // Don't apply this condition in the special zero duration case, since zero is a special value used when the duration
+    // is unknown, and applying the condition in that case may have undesired effects.
+    // See: MediaPlayerPrivateGStreamer::duration(), MediaPlayerPrivateGStreamerMSE::duration().
+    if (duration() != MediaTime::zeroTime() && playbackPosition > duration())
+        playbackPosition = duration();
+
     setCachedPosition(playbackPosition);
     invalidateCachedPositionOnNextIteration();
     return playbackPosition;

kkanag314/WPEWebKit

Source/WebCore/html/HTMLMediaElement.cpp
@@ -5353,8 +5353,11 @@ void HTMLMediaElement::mediaPlayerPlaybackStateChanged()
     beginProcessingMediaPlayerCallback();
     if (playerPaused)
         pauseInternal();
-    else
+    else if (!seeking() || !ended()) {
+        // The extra conditions above prevent autoplay when seeking, which would start playback from 0
+        // and cause a spurious seek to 0 when seeking to the end of the media.
         playInternal();
+    }
     endProcessingMediaPlayerCallback();
 }
 

Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
@@ -1493,6 +1493,14 @@ MediaTime MediaPlayerPrivateGStreamer::playbackPosition() const
     else if (m_canFallBackToLastFinishedSeekPosition)
         playbackPosition = m_seekTime;
 
+    // Avoid exceeding the reported duration even by minuscule amounts.
+    // The multiplatform layer could degrade us to HaveMetadata if we let that happen (e.g. in MSE monitorSourceBuffers()).
+    // Don't apply this condition in the special zero duration case, since zero is a special value used when the duration
+    // is unknown, and applying the condition in that case may have undesired effects.
+    // See: MediaPlayerPrivateGStreamer::duration(), MediaPlayerPrivateGStreamerMSE::duration().
+    if (durationMediaTime() != MediaTime::zeroTime() && playbackPosition > durationMediaTime())
+        playbackPosition = durationMediaTime();
+
     setCachedPosition(playbackPosition);
     invalidateCachedPositionOnNextIteration();
     return playbackPosition;